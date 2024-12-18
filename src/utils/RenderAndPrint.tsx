7ea9f57e5652d5576d10856504faf7a666f8a6e7
KosmicTeal <69432458+kosmicteal@users.noreply.github.com>
2024-12-18 12:36:35 +0100

    fix(print): remove print leftover

--- src/utils/RenderAndPrint.tsx
+++ src/utils/RenderAndPrint.tsx
@@ -3,17 +3,19 @@ import { BasicContent } from '../components/pageComponents/pageGeneric/BasicCont
 import computedStyleToInlineStyle from 'computed-style-to-inline-style';
 import { useReactToPrint } from 'react-to-print';
 import { reduxStore } from '../main';
-import { reduxSlice } from '../redux/slicer';
+import { reduxSelector, reduxSlice } from '../redux/slicer';
 
 export function RenderAndPrint() {
   const contentRef = useRef<HTMLDivElement>(null);
+  const { selectName } = reduxSlice.selectors;
   const { appPrintData } = reduxSlice.actions;
   const { dispatch } = reduxStore;
 
+  const characterName = reduxSelector(selectName);
+
   const handlePrint = useReactToPrint({
-    preserveAfterPrint: true,
     pageStyle: '@media print { html { zoom: 95% } }',
-    documentTitle: 'reactionroll_print',
+    documentTitle: `reActionRoll_${characterName}_print`,
     contentRef,
     onAfterPrint() {
       dispatch(appPrintData());
